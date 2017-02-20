7a1376b4999f1056f3c40206a9f8596e9ea05296
Jairenz Batu <jairenz.batu@toro.io>
2017-02-20 09:56:06 +0800

    Install and Configure NGINX

--- tier_1/recipes/default.rb
+++ tier_1/recipes/default.rb
@@ -2,4 +2,25 @@
 # Cookbook Name:: tier_1
 # Recipe:: default
 #
-# Copyright (c) 2017 TORO Limited 2014-2017, All Rights Reserved.
+# Copyright (c) 2017 TORO Limited, All Rights Reserved.
+
+package 'nginx'
+package 'nginx-mod-stream'
+
+template '/etc/nginx/nginx.conf' do
+        source 'nginx.conf.erb'
+end
+
+service 'nginx' do
+	action [:restart]
+end
+
+execute "Register Server's Public IP" do
+  user "root"
+  command "echo $(curl http://169.254.169.254/latest/meta-data/public-ipv4) > /datastore/configs/saas/web-servers"
+end
+
+execute "Register Server's Private IP" do
+  user "root"
+  command "echo $(curl http://169.254.169.254/latest/meta-data/local-ipv4) > /datastore/configs/saas/web-servers-private"
+end

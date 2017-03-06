571cd7de4998b8a7a1621742da93a195474c08d6
Jairenz Batu <jairenz.batu@toro.io>
2017-03-06 10:47:59 +0800

    CLOUD-959: Mount point added

--- global/recipes/mount_efs.rb
+++ global/recipes/mount_efs.rb
@@ -14,6 +14,9 @@ bash 'mount_efs' do
   user 'root'
   cwd '/'
   code <<-EOH
-	mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).fs-077bbe4e.efs.us-east-1.amazonaws.com:/ /datastore
+  	efsId=$(cat /opt/tmp/efsInstance)
+  	region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+  	availabilityZone=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+	mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${availabilityZone}.${efsId}.efs.${region}.amazonaws.com:/ /datastore
   EOH
 end

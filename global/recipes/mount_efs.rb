c9777715319f1b7fdac6621ba65d47cfa440a076
Jairenz Batu <jairenz.batu@toro.io>
2017-03-08 11:55:33 +0800

    CLOUD-959: Refactor EFS Mount script

--- global/recipes/mount_efs.rb
+++ global/recipes/mount_efs.rb
@@ -4,19 +4,20 @@
 #
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
-directory '/datastore' do
+home_dir = "/datastore"
+efs_id = shell_out("cat /opt/tmp/efsInstance")
+region = shell_out("curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region")
+availability_zone = open("http://169.254.169.254/latest/meta-data/placement/availability-zone")
+
+directory "#{home_dir}" do
   owner 'root'
   group 'root'
   action :create
 end
 
-bash 'mount_efs' do
-  user 'root'
-  cwd '/'
-  code <<-EOH
-  	efsId=$(cat /opt/tmp/efsInstance)
-  	region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
-  	availabilityZone=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-	mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${availabilityZone}.${efsId}.efs.${region}.amazonaws.com:/ /datastore
-  EOH
+mount "#{home_dir}" do
+  device "#{availability_zone}.#{efs_id}.efs.#{region}.amazonaws.com:/"
+  fstype 'nfs'
+  options 'rw,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2'
+  action [:mount]
 end

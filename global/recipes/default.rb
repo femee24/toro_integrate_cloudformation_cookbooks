dc7ee95266696af7a13904540e5c620118f536c3
Jairenz Batu <jairenz.batu@toro.io>
2017-03-09 15:23:13 +0800

    CLOUD-959: Refactor global folders recipe

--- global/recipes/default.rb
+++ global/recipes/default.rb
@@ -6,7 +6,7 @@
 
 # Create default folders
 %w{apps clients configs lab library logs temp trash vhosts}.each do |dir|
-  directory "/datastore/#{dir}" do
+  directory "#{node[:infra][:home_dir]}/#{dir}" do
     mode '0755'
     owner 'root'
     group 'root'

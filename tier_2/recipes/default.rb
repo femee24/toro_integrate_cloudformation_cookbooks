18fdc163d68043565bcbb901868af00552f4d589
Jairenz Batu <jairenz.batu@toro.io>
2017-03-09 09:29:52 +0800

    CLOUD-959: Configure mysql database as necessarily

--- tier_2/recipes/default.rb
+++ tier_2/recipes/default.rb
@@ -6,7 +6,7 @@
 
 include_attribute "global"
 
-#Configuring default folders under assets
+# Configuring default folders under assets
 %w{data jdbc-pool logs packages system-tmp tmp}.each do |dir|
   directory "#{node[:application][:assets_dir]}" do
     mode '0755'
@@ -17,7 +17,7 @@ include_attribute "global"
   end
 end
 
-#Configuring default folders under configs
+# Configuring default folders under configs
 %w{.java custom-web-configs}.each do |dir|
   directory "#{node[:application][:configs_dir]}" do
     mode '0755'
@@ -28,3 +28,14 @@ end
   end
 end
 
+# Configure database
+cookbook_file "/tmp/configure_mysql.sh" do
+  source "mysql-config.sh"
+  mode 0755
+  owner "root"
+end
+
+execute "configure mysql" do
+  user "root"
+  command "sh /tmp/configure_mysql.sh '#{node[:application][:code]}' '#{node[:database][:username]}' '#{node[:database][:password]}' '#{node[:database][:master_user]}' '#{node[:database][:master_pass]}' '#{node[:database][:host]}' > #{node[:infra][:log_dir]}/mysql-config-${HOSTNAME}-$(date +"%m-%d-%y").log"
+end

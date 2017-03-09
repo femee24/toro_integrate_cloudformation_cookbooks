6fc3722fad62a85a41219d7f5cd592a2982d6c28
meltonpineda <melton.pineda@toro.io>
2017-03-09 11:41:52 +0800

    CLOUD-959: Fix unknown type of %string

--- tier_2/recipes/default.rb
+++ tier_2/recipes/default.rb
@@ -43,12 +43,12 @@ end
 
 # Configure database
 cookbook_file "/tmp/configure_mysql.sh" do
-  source "mysql-config.sh"
+  source "configure_mysql.sh"
   mode 0755
   owner "root"
 end
 
 execute "configure mysql" do
   user "root"
-  command "sh /tmp/configure_mysql.sh '#{node[:application][:code]}' '#{node[:database][:username]}' '#{node[:database][:password]}' '#{node[:database][:master_user]}' '#{node[:database][:master_pass]}' '#{node[:database][:host]}' > #{node[:infra][:log_dir]}/mysql-config-${HOSTNAME}-$(date +"%m-%d-%y").log"
+  command "sh /tmp/configure_mysql.sh '#{node[:application][:code]}' '#{node[:database][:username]}' '#{node[:database][:password]}' '#{node[:database][:master_user]}' '#{node[:database][:master_pass]}' '#{node[:database][:host]}' > #{node[:infra][:log_dir]}/mysql-config-${HOSTNAME}-$(date +'%m-%d-%y').log"
 end

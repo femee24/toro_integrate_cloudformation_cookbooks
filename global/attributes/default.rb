c5c9511ec717a57288471afeab4e6c8e3892a945
Jairenz Batu <jairenz.batu@toro.io>
2017-03-09 08:33:46 +0800

    CLOUD-959: Spacing fix

--- global/attributes/default.rb
+++ global/attributes/default.rb
@@ -10,10 +10,10 @@ default[:infra][:home_dir]                            = "/datastore"
 default[:application][:name]                          = "integrate"
 default[:application][:organization]                  = "yourorganization"
 default[:application][:code]                          = "yourorganization1"
-default[:application][:clients_dir]						= "#{node[:infra][:home_dir]}/clients"
-default[:application][:home_dir]							= "#{node[:application][:clients_dir]}/#{node[:application][:organization]}/apps/#{node[:application][:name]}/#{node}[:application][:code]"
-default[:application][:assets_dir]						= "#{node[:application][:home_dir]}/assets"
-default[:application][:configs_dir]						= "#{node[:application][:home_dir]}/configs"
+default[:application][:clients_dir]                   = "#{node[:infra][:home_dir]}/clients"
+default[:application][:home_dir]                      = "#{node[:application][:clients_dir]}/#{node[:application][:organization]}/apps/#{node[:application][:name]}/#{node}[:application][:code]"
+default[:application][:assets_dir]                    = "#{node[:application][:home_dir]}/assets"
+default[:application][:configs_dir]                   = "#{node[:application][:home_dir]}/configs"
 
 # Generate Random Password for the Database
 db_pw = String.new
@@ -22,12 +22,12 @@ while db_pw.length < 20
 end
 
 # Database Related Attributes
-default[:database][:master_user]							= shell_out("cat #{node[:infra][:home_dir]}/temp/dbuser")
-default[:database][:master_pass]							= shell_out("cat #{node[:infra][:home_dir]}/temp/dbpass")
-default[:database][:host]											= shell_out("cat #{node[:infra][:home_dir]}/temp/dbhost")
-default[:database][:username]									= "#{node[:application][:code]}"
-default[:database][:password]									= db_pw
-default[:database][:tracker_db]								= "#{node[:application][:code]}_tracker"
-default[:database][:coder_db]									= "#{node[:application][:code]}_coder"
+default[:database][:master_user]                      = shell_out("cat #{node[:infra][:home_dir]}/temp/dbuser")
+default[:database][:master_pass]                      = shell_out("cat #{node[:infra][:home_dir]}/temp/dbpass")
+default[:database][:host]                             = shell_out("cat #{node[:infra][:home_dir]}/temp/dbhost")
+default[:database][:username]                         = "#{node[:application][:code]}"
+default[:database][:password]                         = db_pw
+default[:database][:tracker_db]                       = "#{node[:application][:code]}_tracker"
+default[:database][:coder_db]                         = "#{node[:application][:code]}_coder"
 
 #

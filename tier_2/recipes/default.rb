7f2f79507e69f5c564596a4a818e5802d5a4401d
Jairenz Batu <jairenz.batu@toro.io>
2017-03-17 13:31:52 +0800

    CLOUD-959: Re-arrange blocks and update comments

--- tier_2/recipes/default.rb
+++ tier_2/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
 
-#Configuring default folders under assets
+#Configure default folders under assets
 %w{data jdbc-pool logs packages system-tmp tmp}.each do |dir|
   directory "#{node[:application][:assets_dir]}/#{dir}" do
     mode '0755'
@@ -16,7 +16,7 @@
   end
 end
 
-# Configuring default folders under configs
+# Configure default folders under configs
 %w{.java custom-web-configs}.each do |dir|
   directory "#{node[:application][:configs_dir]}/#{dir}" do
     mode '0755'
@@ -27,24 +27,28 @@ end
   end
 end
 
-#Configuring default folders under assets
-directory "#{node[:application][:web_config_dir]}"
-
+# toro integrate configuration file
 template "#{node[:application][:assets_dir]}/data/override.properties" do
         source 'override.properties.erb'
         action :create_if_missing
 end
 
+# Configure web directory
+directory "#{node[:application][:web_config_dir]}"
+
+# HTTP/HTTPS Proxy Configuration
 template "#{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}.conf" do
         source 'integrate.conf.erb'
         action :create_if_missing
 end
 
+# HTTP/HTTPS Proxy Configuration
 template "#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}.conf" do
         source 'integrate-ftp-stream.conf.erb'
         action :create_if_missing
 end
 
+# Generate database configuration
 %w{tracker coder}.each do |db|
 template "#{node[:application][:assets_dir]}/jdbc-pool/#{db}.xml" do
         source 'jdbc.xml.erb'
@@ -55,7 +59,7 @@ template "#{node[:application][:assets_dir]}/jdbc-pool/#{db}.xml" do
    end
 end
 
-# Configure database
+# Configure external database
 cookbook_file "/tmp/configure_mysql.sh" do
   source "configure_mysql.sh"
   mode 0755

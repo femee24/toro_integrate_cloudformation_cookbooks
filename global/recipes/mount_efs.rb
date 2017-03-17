7b4683ec527ace254d4bb0262c8f7b8a09710887
meltonpineda <melton.pineda@toro.io>
2017-03-17 10:42:50 +0800

    CLOUD-959: Removed Availability Zone from the Url of EFS

--- global/recipes/mount_efs.rb
+++ global/recipes/mount_efs.rb
@@ -11,7 +11,7 @@ directory "#{node[:infra][:home_dir]}" do
 end
 
 mount "#{node[:infra][:home_dir]}" do
-  device "#{node[:infra][:availability_zone]}.#{node[:infra][:efs_id]}.efs.#{node[:infra][:region]}.amazonaws.com:/"
+  device "#{node[:infra][:efs_id]}.efs.#{node[:infra][:region]}.amazonaws.com:/"
   fstype 'nfs'
   options 'rw,nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2'
   action [:mount]

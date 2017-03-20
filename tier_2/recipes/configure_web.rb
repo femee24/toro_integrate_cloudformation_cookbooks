6ab8317042f6e8376d0f9e1bb200c30b74f3885c
meltonpineda <melton.pineda@toro.io>
2017-03-20 11:34:08 +0800

    CLOUD-959: Use bash to execute multiple commands

--- tier_2/recipes/configure_web.rb
+++ tier_2/recipes/configure_web.rb
@@ -17,12 +17,14 @@ template "#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ft
         action :create_if_missing
 end
 
-execute "append integrate conf" do
-  user "root"
-  command "echo #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf >> #{node[:infra][:home_dir]}/vhosts/include.conf"
-end
-
-execute "append integrate ftp conf" do
-  user "root"
-  command "echo #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf >> #{node[:infra][:home_dir]}/vhosts/stream.conf"
+bash 'append_integrate_conf' do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    #Integrate conf
+    perl -i -ne "print unless /#{node[:application][:code]}-/" #{node[:infra][:home_dir]}/vhosts/include.conf
+    echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/include.conf
+    #Integrate ftp conf
+    perl -i -ne "print unless /#{node[:application][:code]}-/" #{node[:infra][:home_dir]}/vhosts/stream.conf
+    echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/stream.conf
+    EOH
 end

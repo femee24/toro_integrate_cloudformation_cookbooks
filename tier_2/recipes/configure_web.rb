c75f7794b25f023fa3c9af316d63d625cd5e01ac
meltonpineda <melton.pineda@toro.io>
2017-03-20 11:16:56 +0800

    CLOUD-959: Add append integrate cons and ftp conf

--- tier_2/recipes/configure_web.rb
+++ tier_2/recipes/configure_web.rb
@@ -16,3 +16,13 @@ template "#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ft
         source 'integrate-ftp-stream.conf.erb'
         action :create_if_missing
 end
+
+execute "append integrate conf" do
+  user "root"
+  command "echo #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf >> #{node[:infra][:home_dir]}/vhosts/include.conf"
+end
+
+execute "append integrate ftp conf" do
+  user "root"
+  command "echo #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf >> #{node[:infra][:home_dir]}/vhosts/stream.conf"
+end

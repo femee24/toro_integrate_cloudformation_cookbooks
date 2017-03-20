f58d2dd860b6637c959b03f71a1f566d759c6e91
meltonpineda <melton.pineda@toro.io>
2017-03-20 13:19:24 +0800

    CLOUD-FIX: Change to complete name when replacing the integrate conf/s

--- tier_2/recipes/configure_web.rb
+++ tier_2/recipes/configure_web.rb
@@ -20,10 +20,10 @@ end
 bash 'append_integrate_conf' do
   code <<-EOH
     #Integrate conf
-    perl -i -ne "print unless /#{node[:application][:code]}-/" #{node[:infra][:home_dir]}/vhosts/include.conf
+    perl -i -ne "print unless /#{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}-/" #{node[:infra][:home_dir]}/vhosts/include.conf
     echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/include.conf
     #Integrate ftp conf
-    perl -i -ne "print unless /#{node[:application][:code]}-/" #{node[:infra][:home_dir]}/vhosts/stream.conf
+    perl -i -ne "print unless /#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}-/" #{node[:infra][:home_dir]}/vhosts/stream.conf
     echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/stream.conf
     EOH
 end

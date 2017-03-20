ded8f16ace1489824bea9c24108f549650872e60
meltonpineda <melton.pineda@toro.io>
2017-03-20 15:50:36 +0800

    CLOUD-FIX: Changed sed from python for deleting of included lines

--- tier_2/recipes/configure_web.rb
+++ tier_2/recipes/configure_web.rb
@@ -18,10 +18,10 @@ end
 bash 'append_integrate_conf' do
   code <<-EOH
     #Integrate conf
-    perl -i -ne "print unless /#{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}-/" #{node[:infra][:home_dir]}/vhosts/include.conf
+    sed -i '/#{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}/d' #{node[:infra][:home_dir]}/vhosts/include.conf
     echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/include.conf
     #Integrate ftp conf
-    perl -i -ne "print unless /#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}-/" #{node[:infra][:home_dir]}/vhosts/stream.conf
+    sed -i 's/#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}/d' #{node[:infra][:home_dir]}/vhosts/stream.conf
     echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/stream.conf
     EOH
 end

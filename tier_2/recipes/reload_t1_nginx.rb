f23032b793b184f5b6ff94561ad297e49347f667
meltonpineda <melton.pineda@toro.io>
2017-03-21 10:17:59 +0800

    CLOUD-FIX: Change execute to bash resource

--- tier_2/recipes/reload_t1_nginx.rb
+++ tier_2/recipes/reload_t1_nginx.rb
@@ -4,7 +4,8 @@
 #
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
-execute "configure mysql" do
-  user "root"
-  command "aws opsworks create-deployment --region #{node[:infra][:region]} --stack-id #{node[:infra][:t1_stack_id]}  --command "{\"Name\":\"execute_recipes\", \"Args\": {\"recipes\": [\"tier_1::reload_nginx\"]}}"""
+bash 'reload_t1_nginx' do
+  code <<-EOH
+    aws opsworks create-deployment --region #{node[:infra][:region]} --stack-id #{node[:infra][:t1_stack_id]}  --command '{ "Name": "execute_recipes", "Args": {"recipes": ["tier_1::reload_nginx"]}}' aws opsworks create-deployment --region us-east-1 --stack-id 2b3f6d34-b6f5-44e4-8998-09e6da2b48ba  --command '{ "Name": "execute_recipes", "Args": {"recipes": ["tier_1::reload_nginx"]}}'
+    EOH
 end

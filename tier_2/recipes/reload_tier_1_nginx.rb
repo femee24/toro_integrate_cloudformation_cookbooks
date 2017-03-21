0eb9d76414bdaf302129137e034eb99f71de64e6
meltonpineda <melton.pineda@toro.io>
2017-03-21 09:49:19 +0800

    CLOUD-959: Add recipe to reload nginx from t2 to t1

---
+++ tier_2/recipes/reload_tier_1_nginx.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: tier_2
+# Recipe:: reload_tier_1_nginx
+#
+# Copyright (c) 2017 TORO Limited, All Rights Reserved.
+
+execute "configure mysql" do
+  user "root"
+  command "aws opsworks create-deployment --region #{node[:infra][:region]} --stack-id #{node[:infra][:t1_stack_id]}  --command '{ "Name": "execute_recipes", "Args": {"recipes": ["tier_1::reload_nginx"]}}'"
+end

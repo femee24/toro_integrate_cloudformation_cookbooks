3c2eb0f973499d39fc3c4e5eeaea00f7f71b093a
meltonpineda <melton.pineda@toro.io>
2017-03-23 10:44:00 +0800

    CLOUD-FIX: Solr Directory Fix

--- tier_2/recipes/install_solr.rb
+++ tier_2/recipes/install_solr.rb
@@ -4,14 +4,18 @@
 #
 # Copyright (c) 2016 TORO Limited, All Rights Reserved.
 
-directory "#{node[:zookeeper][:installer_dir]}" do
+directory "#{node[:solr][:installer_dir]}" do
     mode '0755'
     recursive true
 end
 
-remote_file "/datastore/apps/solr/installer/solr-#{node[:solr][:version]}.tgz" do
+remote_file "#{node[:solr][:installer_dir]}/solr-#{node[:solr][:version]}.tgz" do
 	source "http://archive.apache.org/dist/lucene/solr/#{node[:solr][:version]}/solr-#{node[:solr][:version]}.tgz"
 	action :create_if_missing
+end
+
+execute "decompress artifact to new instance" do
+  command "tar -zxvf #{node[:solr][:installer_dir]}/solr-#{node[:solr][:version]}.tgz -C /opt"
 end
 
 execute "start solr" do

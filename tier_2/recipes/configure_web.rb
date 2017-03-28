9c53840a38b1c8f3b34d60aea0a78f9e56e7c563
meltonpineda <melton.pineda@toro.io>
2017-03-28 15:58:22 +0800

    CLOUD-FIX: Change bash resource to execute resource

--- tier_2/recipes/configure_web.rb
+++ tier_2/recipes/configure_web.rb
@@ -15,13 +15,23 @@ template "#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ft
         source 'integrate-ftp-stream.conf.erb'
 end
 
-bash 'append_integrate_conf' do
-  code <<-EOH
-    #Integrate conf
-    sed -i '/#{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}/d' #{node[:infra][:home_dir]}/vhosts/include.conf
-    echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/include.conf
-    #Integrate ftp conf
-    sed -i 's/#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}/d' #{node[:infra][:home_dir]}/vhosts/stream.conf
-    echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/stream.conf
-    EOH
+# bash 'append_integrate_conf' do
+#   code <<-EOH
+#     #Integrate conf
+#     sed -i '/#{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}/d' #{node[:infra][:home_dir]}/vhosts/include.conf
+#     echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/include.conf
+#     #Integrate ftp conf
+#     sed -i 's/#{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}/d' #{node[:infra][:home_dir]}/vhosts/stream.conf
+#     echo "include #{node[:application][:web_config_dir]}/#{node[:application][:code]}-ftp-#{node[:application][:name]}-#{node[:opsworks][:instance][:hostname]}.conf;" >> #{node[:infra][:home_dir]}/vhosts/stream.conf
+#     EOH
+# end
+
+execute "append integrate conf" do
+  command "echo 'include '#{node[:application][:web_config_dir]}'/'#{node[:application][:code]}'-'#{node[:application][:name]}'-'#{node[:opsworks][:instance][:hostname]}'.conf;' >> #{node[:infra][:home_dir]}/vhosts/include.conf"
+  only_if "[[ -z $(grep -oE #{node[:application][:code]} #{node[:infra][:home_dir]}/vhosts/include.conf ) ]]"
+end
+
+execute "append integrate ftp conf" do
+  command "echo 'include '#{node[:application][:web_config_dir]}'/'#{node[:application][:code]}'-ftp-'#{node[:application][:name]}'-'#{node[:opsworks][:instance][:hostname]}'.conf;' >> #{node[:infra][:home_dir]}/vhosts/stream.conf"
+  only_if "[[ -z $(grep -oE #{node[:application][:code]} #{node[:infra][:home_dir]}/vhosts/stream.conf ) ]]"
 end

a3d4ae0a9dc9b0f6f436349d16ca039e02290895
meltonpineda <melton.pineda@toro.io>
2017-03-30 10:05:58 +0800

    CLOUD-FIX: Changed the formatting

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -5,8 +5,8 @@
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
 
-node['opsworks']['layers']['zookeeper']['instances'].each do |instance, instancedata|
-    log "Private IP: #{node[:opsworks][:layers][:zookeeper][:instances][:instance][:private_ip]}"
+"#{node[:opsworks][:layers][:zookeeper][:instances]}".each do |instance, instancedata|
+    log "Private IP: #{node[:opsworks][:layers][:zookeeper][:instances][instance][:private_ip]}"
   end
 end
 

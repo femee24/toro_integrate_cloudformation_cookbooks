cdcc3ae52576fc50d65b2842e9e8ba2aaffba476
meltonpineda <melton.pineda@toro.io>
2017-03-30 16:22:37 +0800

    CLOUD-FIX:  Increment server number

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -4,8 +4,7 @@
 #
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
-i = 0
-
 node['opsworks']['layers']['zookeeper']['instances'].each do |instance, instancedata|
-  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i+1}=#{instancedata['private_ip']}:2888:3888\n" }
+  i += 1
+  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i}=#{instancedata['private_ip']}:2888:3888\n" }
 end

2de6a1906f0c08a0cdf3d5780a85dd9b5749e89a
meltonpineda <melton.pineda@toro.io>
2017-03-30 11:01:56 +0800

    CLOUD-TEST: Looping of ip addresses

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -9,8 +9,7 @@
 #    log "Private IP: #{node[:opsworks][:layers][:zookeeper][:instances][instance][:private_ip]}"
 #  end
 #end
-ec2 = "zookeeper1"
 
-file "/tmp/zk" do
-  content "#{node[:opsworks][:layers][:zookeeper][:instances][ec2][:ip]}"
+"#{node[:opsworks][:layers][:zookeeper][:instances]}".each do |instance|
+  content "#{node[:opsworks][:layers][:zookeeper][:instances][instance][:ip]}"
 end

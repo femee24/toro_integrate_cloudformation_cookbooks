fa0b356ec3be0c3318cd61e59b8395dceb35d14f
meltonpineda <melton.pineda@toro.io>
2017-03-30 16:38:31 +0800

    CLOUD-TEST: increment test

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -7,6 +7,7 @@
 i = 0
 
 node['opsworks']['layers']['zookeeper']['instances'].each do |instance, instancedata|
-  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i + 1}=#{instancedata['private_ip']}:2888:3888\n" }
+  i = i + 1
+  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i}=#{instancedata['private_ip']}:2888:3888\n" }
   log "current server number is #{i}"
 end

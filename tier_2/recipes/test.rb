b877a2d708be7501e40d9771fcec574e55004047
meltonpineda <melton.pineda@toro.io>
2017-03-30 16:05:17 +0800

    CLOUD-FIX: add new line

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -6,6 +6,5 @@
 
 
 node['opsworks']['layers']['zookeeper']['instances'].each do |instance, instancedata|
-  i = 0
-  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i+1}=#{instancedata['private_ip']}:2888:3888" }
+  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i+1}=#{instancedata['private_ip']}:2888:3888\n" }
 end

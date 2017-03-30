287c68f6d2948ad4c40c5fdb19acc44678bedd9e
meltonpineda <melton.pineda@toro.io>
2017-03-30 10:25:13 +0800

    CLOUD-TEST: try printing a single ip

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -5,9 +5,9 @@
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
 
-"#{node[:opsworks][:layers][:zookeeper][:instances]}".each do |instance, instancedata|
-    log "Private IP: #{node[:opsworks][:layers][:zookeeper][:instances][instance][:private_ip]}"
-  end
-end
+#"#{node[:opsworks][:layers][:zookeeper][:instances]}".each do |instance, instancedata|
+#    log "Private IP: #{node[:opsworks][:layers][:zookeeper][:instances][instance][:private_ip]}"
+#  end
+#end
 
-#file('/opt/hosts').must_include "#{Resolv.getaddress(instance_config[:private_ip])} #{instance_name}"
+log "node["opsworks"]["layers"]["zookeeper"]["instances"]["zookeeper1"]["ip"]"

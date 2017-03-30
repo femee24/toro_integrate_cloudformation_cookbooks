da85b21ddbe11f729e74e2866feafc2e43d684c3
meltonpineda <melton.pineda@toro.io>
2017-03-30 09:58:50 +0800

    CLOUD-TEST: Completed Variable name

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -5,7 +5,9 @@
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
 
-node['opsworks']['layers']['zookeeper']["instances"].each do |instance, instancedata|
-    log "Private IP: #{instancedata['private_ip']}"
+node['opsworks']['layers']['zookeeper']['instances'].each do |instance, instancedata|
+    log "Private IP: #{node[:opsworks][:layers][:zookeeper][:instances][:instance][:private_ip]}"
   end
 end
+
+#file('/opt/hosts').must_include "#{Resolv.getaddress(instance_config[:private_ip])} #{instance_name}"

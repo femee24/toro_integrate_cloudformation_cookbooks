3c761b02def1ddd101cb0a90c948e8b2997076bc
meltonpineda <melton.pineda@toro.io>
2017-03-30 15:59:41 +0800

    CLOUD-959: Append servers ip to a file

--- tier_2/recipes/test.rb
+++ tier_2/recipes/test.rb
@@ -7,5 +7,5 @@
 
 node['opsworks']['layers']['zookeeper']['instances'].each do |instance, instancedata|
   i = 0
-  file("tmp/zoo.cfg").must_include "server.#{i+1}=#{instancedata['private_ip']}:2888:3888"
+  open("tmp/zoo.cfg", 'a') { |f| f <<  "server.#{i+1}=#{instancedata['private_ip']}:2888:3888" }
 end

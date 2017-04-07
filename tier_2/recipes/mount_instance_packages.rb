0d0b54b7495870c516098814dbe4a51fe999c33b
meltonpineda <melton.pineda@toro.io>
2017-04-07 09:41:37 +0800

    CLOUD-FIX: Add recursive when creating integrate packages

--- tier_2/recipes/mount_instance_packages.rb
+++ tier_2/recipes/mount_instance_packages.rb
@@ -5,7 +5,9 @@
 # Copyright (c) 2017 TORO Limited, All Rights Reserved.
 
 # Create packages mount point
-directory "#{node[:application][:packages]}"
+directory "#{node[:application][:packages]}" do
+  recursive true
+end
 
 mount "#{node[:application][:packages]}" do
   device "#{node[:infra][:efs_id]}.efs.#{node[:infra][:region]}.amazonaws.com:/#{node[:application][:assets_dir]}/packages/#{node[:opsworks][:instance][:hostname]}"

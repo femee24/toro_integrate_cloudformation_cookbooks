102a1d780f9ff2fd332db9e0d45aab7517da4f2d
Melton Pineda <meltonpineda@ip-192-168-21-173.ec2.internal>
2017-06-19 13:30:20 +0800

    CLOUD-959: adding monitoring recipes

---
+++ global/recipes/cloudwatch_alarms.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: toro_global
+# Recipe:: cloudwatch_alarms
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+Statistic= "Average"
+Unit= "Percent"
+Threshold= 80
+Period= 300
+ComparisonOperator= "GreaterThanThreshold"
+EvaluationPeriods= 1
+Actions="#{node[:infra][:notification_email]}"
+NameSpace= "System/Linux"
+Dimensions= "Name=InstanceId,Value=#{node[:opsworks][:instance][:aws_instance_id]}"
+
+['cpu', 'mem', 'disk'].each do |check|
+  case [check]
+    when ["cpu"]
+      AlarmName= "#{node[:opsworks][:instance][:hostname]}-#{node[:infra][:resource_group]}-#{check}-gt-#{Threshold}-5min"
+      MetricName= "CPUUtilization"
+      NameSpace= "AWS/EC2"
+    when ["mem"]
+      AlarmName= "#{node[:opsworks][:instance][:hostname]}-#{node[:infra][:resource_group]}-#{check}-gt-#{Threshold}-5min"
+      MetricName= "MemoryUtilization"
+      NameSpace= "System/Linux"
+    when ["disk"]
+      AlarmName= "#{node[:opsworks][:instance][:hostname]}-#{node[:infra][:resource_group]}-#{check}-gt-#{Threshold}-5min"
+      MetricName= "DiskSpaceUtilization"
+      NameSpace= "System/Linux"
+      Dimensions= "Name=Filesystem,Value=/dev/xvda1 Name=MountPath,Value=/ Name=InstanceId,Value=#{node[:opsworks][:instance][:aws_instance_id]}"
+
+    else
+      log "wrong utilization parameter"
+  end
+  execute check do
+    command "aws cloudwatch put-metric-alarm --region us-east-1 --alarm-name #{AlarmName} --metric-name #{MetricName} --namespace #{NameSpace} --statistic #{Statistic} --unit #{Unit} --period #{Period} --threshold #{Threshold} --comparison-operator #{ComparisonOperator} --dimensions #{Dimensions} --evaluation-periods #{EvaluationPeriods} --ok-actions #{Actions} --alarm-actions #{Actions}"
+  end
+end
---
+++ global/recipes/monitor_resources.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: toro_global
+# Recipe:: monitor_resources
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+['perl-Switch', 'perl-DateTime', 'perl-Sys-Syslog', 'perl-LWP-Protocol-https'].each do |p|
+package p do
+	action :install
+  end
+end
+
+remote_file "/opt/CloudWatchMonitoringScripts-1.2.1.zip" do
+	source "http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip"
+	action :create_if_missing
+end
+
+execute "unzip_cloudwatchmonitoring_scripts" do
+  command "unzip -o /opt/CloudWatchMonitoringScripts-1.2.1.zip -d /opt/"
+end
+
+cron 'put_mem_disk_data' do
+  action :create
+  minute '*/5'
+  command "/opt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --disk-space-util --disk-path=/ --from-cron"
+end
